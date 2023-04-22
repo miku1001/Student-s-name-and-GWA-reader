56724da4009acbee93cdfae627ddc17977179c61
miku1001 <pauloted.22@gmail.com>
2023-04-23 00:13:50 +0800

    Pseudocodes

---
+++ name_gwa_reader.py
@@ -0,0 +1,16 @@
+# Write a Python program that read a file containing the name of 20 students together 
+# with their GWA. The program will outputs the name of the student who got the highest GWA (including the GWA).
+
+# Open student_name_and_gwa.txt(read)
+# Create an empty dictionary to put gathered name and their GWA
+# Read every lines in file
+# Split every lines in 2 parts to get the name and GWA
+# Extract the GWA and name from splitted line
+# Convert the GWA into integer
+# Append the name and dictionary on the created dictionary
+# Print student's name and GWA
+
+# To print the student who got highest score,
+# Create a list where you'll put the name of highest student
+# Get the highest GWA
+# Get the name of student
