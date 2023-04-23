5424455983d806a2478b1019d7bc5d4789addf74
miku1001 <pauloted.22@gmail.com>
2023-04-23 20:14:36 +0800

    Change grading scale (1-highest 5-lowest) 2

--- name_gwa_reader.py
+++ name_gwa_reader.py
@@ -17,11 +17,11 @@ def student():
                 # Get the GWA
                 gwa = lines[1].strip()
                 # Convert the GWA into float
-                gwa_int = float(gwa)
+                gwa_float = float(gwa)
                 # Append the name and dictionary on the created dictionary
-                student_dict[name] = gwa_int
+                student_dict[name] = gwa_float
                 # Print student's name and GWA
-                print(f"\033[34mStudent's name:\033[0m {name:<10s} \033[36mGWA:\033[0m {gwa_int:>1.5f}")
+                print(f"\033[34mStudent's name:\033[0m {name:<10s} \033[36mGWA:\033[0m {gwa_float:>1.5f}")
 
     # To print the student who got highest score,
     # Create a list where you'll put the name of highest student
